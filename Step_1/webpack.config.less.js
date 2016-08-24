6e0710f9ad7eef82767fb68b752feb2d010f3d28
Seppo <hellozhanagran@hotmail.com>
2016-08-24 09:06:43 +0800

    test

--- Step_1/webpack.config.less.js
+++ Step_1/webpack.config.less.js
@@ -1,11 +1,13 @@
 /**
  * 开始讲Loader
- * 1. 安装最基本的css-loader: npm install --save-dev css-loader style-loader
+ * 1. npm install extract-text-webpack-plugin --save-dev
  * 2. 按下面的配置写，然后执行webpack
  * 3. main.js中的require('style.css')会被加载到js文件中。
  * 4. 如果我们想用less，而且是单独编译css文件，看webpack.config.less.js
  *
  */
+
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = {
     entry:__dirname + '/main.js',//这个地方记得不能是'./main.js'
     output:{
@@ -14,7 +16,11 @@ module.exports = {
     },
     module:{
         loaders:[
-            {test:/\.css$/,loader:'style!css'}
+            {test:/\.less$/,loader:ExtractTextPlugin.extract('style!css!less')}
         ]
-    }
+    },
+    plugins:[
+        new ExtractTextPlugin("[name].css")
+    ]
+
 }
